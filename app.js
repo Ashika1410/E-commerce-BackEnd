b82d5f9530cb125d45db3e268e5640f3a97e5807
Ashika M <ashika04m@gmail.com>
2025-06-25 11:57:14 +0530

    branch commit

--- app.js
+++ app.js
@@ -1,56 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-
-// const productRoute = require('./Routes/productRoute');
-// const categoryRoute = require('./Routes/categoryRoute');
-// const cartRoute = require('./Routes/cartRoute');
-// const wishlistRoute = require('./Routes/wishlistRoute');
-// const userRoute = require('./Routes/userRoute');
-// const orderRoute = require('./Routes/orderRoute');
-// const loginRoute = require('./Routes/loginRoute');
-// // const paymentRoute = require('./Routes/paymentRoute');
-
-// const app = express();
-// app.use(express.static("dist"));
-// app.use(bodyParser.json());
-
-// // const allowedOrigins = [
-// //   'http://localhost:5174',
-// //   'https://e-commerce-back-e6zpxi0ru-ashika-ms-projects.vercel.app'
-// // ];
-
-// // app.use(cors({
-// //   origin: (origin, callback) => {
-// //     if (!origin || allowedOrigins.includes(origin)) {
-// //       callback(null, true);
-// //     } else {
-// //       callback(new Error('Not allowed by CORS'));
-// //     }
-// //   },
-// //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// //   credentials: false
-// // }));
-
-// app.use(cors({
-//   origin: [`https://e-commerce-back-e6zpxi0ru-ashika-ms-projects.vercel.app`],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }));
-
-// app.use('/product', productRoute);
-// app.use('/category', categoryRoute);
-// app.use('/cart', cartRoute);
-// app.use('/wishlist', wishlistRoute);
-// app.use('/user', userRoute);
-// app.use('/order', orderRoute);
-// app.use('/login', loginRoute);
-// // app.use('/payment', paymentRoute);
-
-// app.listen(7702, () => {
-//     console.log('Project is running on port 7702');
-// });
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -63,8 +10,6 @@ const cartRoute = require('./Routes/cartRoute');
 const wishlistRoute = require('./Routes/wishlistRoute');
 const userRoute = require('./Routes/userRoute');
 const orderRoute = require('./Routes/orderRoute');
-// const loginRoute = require('./Routes/loginRoute');
-// const paymentRoute = require('./Routes/paymentRoute');
 
 const app = express();
 app.use(express.static("dist"));
@@ -72,16 +17,14 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use('/product', productRoute);
-app.use('/category', categoryRoute);
-app.use('/cart', cartRoute);
-app.use('/wishlist', wishlistRoute);
-app.use('/user', userRoute);
-app.use('/order', orderRoute);
-// app.use('/login', loginRoute);
-// app.use('/payment', paymentRoute);
+app.use('/api/product', productRoute);
+app.use('/api/category', categoryRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/wishlist', wishlistRoute);
+app.use('/api/user', userRoute);
+app.use('/api/order', orderRoute);
 
-const PORT = process.env.PORT || 7702;
+const PORT = 7702;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
