f80dbc69aa1e1f85b5d7db42b91b56ef4461fa64
Ashika M <ashika04m@gmail.com>
2025-07-23 12:51:58 +0530

    database url

--- Resources/db.js
+++ Resources/db.js
@@ -1,12 +1,6 @@
 const mysql = require('mysql2');
+require("dotenv").config();
 
-const mysql_Pool = mysql.createPool({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
-    port : process.env.DB_PORT,
-    ssl : false
-});
+const mysql_Pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = {mysql_Pool}; 
