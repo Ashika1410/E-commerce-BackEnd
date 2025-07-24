588075bfbeb697738c5c13ca3f0b95e2a3ce0f63
Ashika M <ashika04m@gmail.com>
2025-07-24 12:52:39 +0530

    new db changed

--- Repoistory/category.js
+++ Repoistory/category.js
@@ -1,44 +1,44 @@
 const mysql_Pool = require('../Resources/db').mysql_Pool;
 
 exports.getAllCategory = () => {
-  return new Promise ((resolve,reject) => {
-  let sqlQuery = `SELECT * FROM category;`;
-  console.log(sqlQuery);
-  mysql_Pool.getConnection((err,con) => {
-    if (err) {
-      return reject (new Error (err));
-    }
-    else {
-      con.query(sqlQuery, (err, result) => {
-        con.release();
-        if (err) {
-          return reject (new Error (err));
-        }
-        else {
-          return resolve ({result:result});
-        }
-      });
-    }
-  });
-  });
-}
-
-exports.getCategory = () => {
-  return new Promise ((resolve,reject) => {
-    let sqlQuery = `SELECT * FROM category WHERE categoryID=2;`;
+  return new Promise((resolve, reject) => {
+    let sqlQuery = `SELECT * FROM category;`;
     console.log(sqlQuery);
-    mysql_Pool.getConnection((err,con) => {
+    mysql_Pool.getConnection((err, con) => {
       if (err) {
-        return reject (new Error (err));
+        return reject(new Error(err));
       }
       else {
         con.query(sqlQuery, (err, result) => {
           con.release();
           if (err) {
-            return reject (new Error (err));
+            return reject(new Error(err));
           }
           else {
-            return resolve ({result:result});
+            return resolve({ result: result });
+          }
+        });
+      }
+    });
+  });
+}
+
+exports.getCategory = () => {
+  return new Promise((resolve, reject) => {
+    let sqlQuery = `SELECT * FROM category WHERE categoryID=2;`;
+    console.log(sqlQuery);
+    mysql_Pool.getConnection((err, con) => {
+      if (err) {
+        return reject(new Error(err));
+      }
+      else {
+        con.query(sqlQuery, (err, result) => {
+          con.release();
+          if (err) {
+            return reject(new Error(err));
+          }
+          else {
+            return resolve({ result: result });
           }
         });
       }
@@ -47,71 +47,71 @@ exports.getCategory = () => {
 }
 
 exports.newCategory = (category, description, img) => {
-  return new Promise ((resolve, reject) => {
-    const sqlQuery =`INSERT INTO category (category_name, category_description, category_imgSrc) VALUES (?, ?, ?);`;
+  return new Promise((resolve, reject) => {
+    const sqlQuery = `INSERT INTO category (category_name, category_description, category_imgSrc) VALUES (?, ?, ?);`;
     const values = [category, description, img];
     console.log(sqlQuery);
     console.log("Sql:", sqlQuery);
-    console.log("Values",values);
-    
+    console.log("Values", values);
+
     mysql_Pool.getConnection((err, con) => {
       if (err) {
-        return reject (new Error (err));
+        return reject(new Error(err));
       }
       else {
         con.query(sqlQuery, values, (err, result) => {
           con.release();
           if (err) {
-            return reject (new Error (err));
+            return reject(new Error(err));
           }
           else {
-            return resolve ({result:result});
+            return resolve({ result: result });
           }
         });
       }
     });
   });
 };
-  
+
 exports.putCategory = (req, res) => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const sqlQuery = `UPDATE category SET category_name='Ornaments' WHERE categoryID=202`;
     console.log(sqlQuery);
     mysql_Pool.getConnection((err, con) => {
-        if (err) {
-          return reject (new Error (err));
-        }
-        else {
-          con.query(sqlQuery, (err, result) => {
-            con.release();
-            if (err) {
-              return reject (new Error (err));
-            }
-            else {
-              return resolve ({result:result});
-            }
-          });
-        }
-      });
-  });
-};
-
-exports.deleteCategory = (req, res) => {
-  return new Promise ((resolve, reject) => {
-    const sqlQuery = `DELETE FROM category WHERE categoryID=206;`;
-    console.log(sqlQuery);
-    mysql_Pool.getConnection((err, con) => {
       if (err) {
-        return reject (new Error (err));
+        return reject(new Error(err));
       }
       else {
         con.query(sqlQuery, (err, result) => {
           con.release();
           if (err) {
-            return reject (new Error (err));
+            return reject(new Error(err));
           }
           else {
-            return resolve ({result:result});
+            return resolve({ result: result });
+          }
+        });
+      }
+    });
+  });
+};
+
+exports.deleteCategory = (req, res) => {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = `DELETE FROM category WHERE categoryID=206;`;
+    console.log(sqlQuery);
+    mysql_Pool.getConnection((err, con) => {
+      if (err) {
+        return reject(new Error(err));
+      }
+      else {
+        con.query(sqlQuery, (err, result) => {
+          con.release();
+          if (err) {
+            return reject(new Error(err));
+          }
+          else {
+            return resolve({ result: result });
           }
         });
       }
--- Resources/db.js
+++ Resources/db.js
@@ -1,6 +1,15 @@
 const mysql = require('mysql2');
 require("dotenv").config();
 
-const mysql_Pool = mysql.createPool(process.env.DATABASE_URL);
+const mysql_Pool = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: Number(process.env.MYSQLPORT),
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-module.exports = {mysql_Pool}; 
+module.exports = { mysql_Pool }; 
